b4cf04a289fed01cc4af2a4edf2220cab5de4cf1
saanvi-arch <saanvi.choudhary2000@gmail.com>
2025-02-28 16:59:54 +0530

    demo.sql

--- dbtlearn/models/mart/demo.sql
+++ dbtlearn/models/mart/demo.sql
@@ -1,10 +1,11 @@
 {{ config(
     materialized='table',
-    schema='MART'  -- Ensure it goes into the correct schema
+    schema='MART'  # No comment inside config()
 ) }}
 
 SELECT 
     1 AS id, 
     'Demo Name' AS name, 
     CURRENT_TIMESTAMP AS created_at
+
 
