868bd32773a0abac03fae8f24a2ee12de805f2ab
saanvi-arch <saanvi.choudhary2000@gmail.com>
2025-03-04 17:41:16 +0530

    updated yml and schema_name.sql

--- dbtlearn/macros/schema_name.sql
+++ dbtlearn/macros/schema_name.sql
@@ -1,7 +1,8 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
+    {# Use 'lake' or 'mart' as provided, without prefixing RAW_ #}
     {% if custom_schema_name is none %}
-        {{ target.schema }}  -- Use the default schema from profiles.yml
+        {{ target.schema }}  
     {% else %}
-        {{ custom_schema_name | lower }}  -- Use the schema from project.yml
+        {{ custom_schema_name }}  -- Forces exact schema from model config
     {% endif %}
 {% endmacro %}
